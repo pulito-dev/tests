aee8e1abae628dc95635378af19687e930a88c59
isakal <sakalivan4@gmail.com>
2024-12-29 18:12:56 +0100

    Remove unused locust tests and fix k6 tests

--- load_tests_k6/script.js
+++ load_tests_k6/script.js
@@ -2,10 +2,20 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  // A number specifying the number of VUs to run concurrently.
-  vus: 100,
-  // A string specifying the total duration of the test run.
-  duration: '10s'
+  thresholds: {
+      http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+      http_req_duration: ['p(95)<500'], // 95% of requests should be below 500ms
+  },
+  scenarios: {
+      my_scenario1: {
+        executor: 'constant-arrival-rate',
+        duration: '10m', // total duration
+        preAllocatedVUs: 1000, // to allocate runtime resources
+  
+        rate: 1000, // number of constant iterations given `timeUnit`
+        timeUnit: '1s',
+      },
+    },
 };
 
 // The function that defines VU logic.
@@ -14,7 +24,7 @@ export const options = {
 // about authoring k6 scripts.
 //
 export default function() {
-  const res = http.get('http://localhost:8000/accommodations/');
+  const res = http.get('http://localhost/listings/');
   check(res, {
     "is status == 200": (r) => r.status == 200
   })
--- load_tests_locust/locustfile.py
+++
@@ -1,9 +0,0 @@
-from locust import HttpUser, task, between
-
-class TestUser(HttpUser):
-    host = "http://localhost:8000"
-    wait_time = between(1, 3)
-
-    @task
-    def view_accommodation(self):
-        self.client.get("/accommodations/")
